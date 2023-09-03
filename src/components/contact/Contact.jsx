cfb7ddc02ae27751fee8660a04f0718c37cffdc6
Bharat <bharatlakhera295@gmail.com>
2023-09-03 20:52:18 +0530

    change

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,6 @@
 
-import { Box, styled, Typography, Link } from '@mui/material';
-import { GitHub, Instagram, Email } from '@mui/icons-material';
-
+import { Box, styled } from '@mui/material';
+ 
 const Banner = styled(Box)`
     background-image: url(http://mrtaba.ir/image/bg2.jpg);
     width: 100%;
@@ -10,35 +9,13 @@ const Banner = styled(Box)`
     background-size: cover;
 `;
 
-const Wrapper = styled(Box)`
-    padding: 20px;
-    & > h3, & > h5 {
-        margin-top: 50px;
-    }
-`;
-
-const Text = styled(Typography)`
-    color: #878787;
-`;
-
+ 
 
 const Contact = () => {
     return (
         <Box>
             <Banner />
-            {/* <Wrapper>
-                <Typography variant="h3">Getting in touch is easy!</Typography>    
-                <Text variant="h5">
-                    Reach out to me on
-                    <Link href="https://www.instagram.com/codeforinterview/" color="inherit" target="_blank">
-                        <Instagram/>
-                    </Link>
-                    or send me an Email 
-                    <Link href="mailto:codeforinterview@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
-                        <Email />
-                    </Link>.
-                </Text>
-            </Wrapper> */}
+  
            Welcome to Contact Section.
         </Box>
     );
